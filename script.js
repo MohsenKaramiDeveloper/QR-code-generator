5daa264f0c230fc23333c1a24872e0319084fb1f
Mohsen Karami <kara0158@algonquinlive.com>
2024-06-05 21:42:08 -0400

    debug remove hidden

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const generateBtn = document.querySelector('form button');
 const qrCodeBox = document.querySelector('.qr-code');
 const qrInput = document.querySelector('form input');
-const qrImage = document.querySelector('.qr-come img');
+const qrImage = document.querySelector('.qr-code img');
 
 
 generateBtn.addEventListener('click' , () => {
